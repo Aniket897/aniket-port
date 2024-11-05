be8147108463120e7688f0b883224639218fab4d
aniket-kangane <aniketkangane@pepsales.co>
2024-11-05 09:04:12 +0530

    smooth scroll added

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,27 @@
+"use client";
+
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import { Button } from "@/components/ui/button";
-
+import { ReactLenis } from "@studio-freight/react-lenis";
 export default function Home() {
   return (
-    <div className="w-[90vw] mx-auto lg:w-[60vw] space-y-20">
-      <Hero />
-      <About />
-      <div className="pt-40">
-        <Projects />
+    <ReactLenis
+      root
+      options={{ lerp: 0, duration: 2, smoothWheel: true, syncTouch: true }}
+    >
+      <div className="w-[90vw] mx-auto lg:w-[60vw] space-y-20">
+        <Hero />
+        <About />
+        <div className="pt-40">
+          <Projects />
+        </div>
+        <Contact />
+        <Footer />
       </div>
-      <Contact />
-      <Footer />
-    </div>
+    </ReactLenis>
   );
 }
